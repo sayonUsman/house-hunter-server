84902e82aec36510ad0200513db4eac8cdc910ec
Sayon Usman <sayonusman.ma@gmail.com>
2023-07-18 16:40:52 +0300

    Added code to encrypt user password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,27 +12,44 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+// get database uri
 const uri = `${process.env.DB_URI}`;
-main().catch((err) => console.log(err));
 
+// connect to database
 async function main() {
   await mongoose.connect(uri);
 }
 
-app.get("/", (req, res) => {
-  res.send("House Hunters' Server is Running.");
+main().catch((err) => console.log(err));
+
+// save new user info to database
+app.post("/signup", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const newUser = new NewUser({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: hashedPassword,
+      role: req.body.role,
+    });
+
+    await newUser
+      .save()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-app.post("/users", async (req, res) => {
-  const newUser = new NewUser(req.body);
-  await newUser
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
+app.get("/", (req, res) => {
+  res.send("House Hunters' Server is Running.");
 });
 
 app.listen(port, () => {
